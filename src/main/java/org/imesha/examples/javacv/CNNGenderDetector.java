916c40853e0dfed0dfae56241f8275427934c838
pragathitr <56511929+pragathitr@users.noreply.github.com>
2020-04-07 18:06:51 +0530

    Add files via upload

---
+++ src/main/java/org/imesha/examples/javacv/CNNGenderDetector.java
@@ -0,0 +1,76 @@
+package org.imesha.examples.javacv;
+
+import org.bytedeco.javacpp.indexer.Indexer;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_dnn.Net;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+import static org.bytedeco.opencv.global.opencv_core.NORM_MINMAX;
+import static org.bytedeco.opencv.global.opencv_core.normalize;
+import static org.bytedeco.opencv.global.opencv_dnn.blobFromImage;
+import static org.bytedeco.opencv.global.opencv_dnn.readNetFromCaffe;
+import static org.bytedeco.opencv.global.opencv_imgproc.resize;
+
+
+public class CNNGenderDetector {
+
+    private static final Logger logger = LoggerFactory.getLogger(CNNGenderDetector.class);
+
+    private Net genderNet;
+
+    public CNNGenderDetector() {
+        try {
+            genderNet = new Net();
+            File protobuf = new File(getClass().getResource("/caffe/deploy_gendernet.prototxt").toURI());
+            File caffeModel = new File(getClass().getResource("/caffe/gender_net.caffemodel").toURI());
+            genderNet = readNetFromCaffe(protobuf.getAbsolutePath(), caffeModel.getAbsolutePath());
+        } catch (Exception e) {
+            logger.error("Error reading prototxt", e);
+            throw new IllegalStateException("Unable to start CNNGenderDetector", e);
+        }
+    }
+
+    /**
+     * Predicts gender of a given cropped face
+     *
+     * @param face  the cropped face as a {@link Mat}
+     * @param frame the original frame where the face was cropped from
+     * @return Gender
+     */
+    public Gender predictGender(Mat face, Frame frame) {
+        try {
+            Mat croppedMat = new Mat();
+            resize(face, croppedMat, new Size(256, 256));
+            normalize(croppedMat, croppedMat, 0, Math.pow(2, frame.imageDepth), NORM_MINMAX, -1, null);
+
+            Mat inputBlob = blobFromImage(croppedMat);
+            genderNet.setInput(inputBlob, "data", 1.0, null);      //set the network input
+
+            Mat prob = genderNet.forward("prob");
+
+            Indexer indexer = prob.createIndexer();
+            logger.debug("CNN results {},{}", indexer.getDouble(0, 0), indexer.getDouble(0, 1));
+            if (indexer.getDouble(0, 0) > indexer.getDouble(0, 1)) {
+                logger.debug("Male detected");
+                return Gender.MALE;
+            } else {
+                logger.debug("Female detected");
+                return Gender.FEMALE;
+            }
+        } catch (Exception e) {
+            logger.error("Error when processing gender", e);
+        }
+        return Gender.NOT_RECOGNIZED;
+    }
+
+    public enum Gender {
+        MALE,
+        FEMALE,
+        NOT_RECOGNIZED
+    }
+}
---
+++ src/main/java/org/imesha/examples/javacv/JavaCVExample.java
@@ -0,0 +1,164 @@
+package org.imesha.examples.javacv;
+
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Point;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.imesha.examples.javacv.util.ImageUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+
+
+public class JavaCVExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(JavaCVExample.class);
+
+    private FrameGrabber frameGrabber;
+    private OpenCVFrameConverter.ToMat toMatConverter = new OpenCVFrameConverter.ToMat();
+    private volatile boolean running = false;
+
+    private HaarFaceDetector faceDetector = new HaarFaceDetector();
+    private CNNAgeDetector ageDetector = new CNNAgeDetector();
+    private CNNGenderDetector genderDetector = new CNNGenderDetector();
+
+    private JFrame window;
+    private JPanel videoPanel;
+
+    public JavaCVExample() {
+        window = new JFrame();
+        videoPanel = new JPanel();
+
+        window.setLayout(new BorderLayout());
+        window.setSize(new Dimension(1280, 720));
+        window.add(videoPanel, BorderLayout.CENTER);
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                stop();
+            }
+        });
+    }
+
+    /**
+     * Starts the frame grabbers and then the frame processing. Grabbed and processed frames will be displayed in the
+     * {@link #videoPanel}
+     */
+    public void start() {
+       // frameGrabber = new FFmpegFrameGrabber("/dev/video0");
+        // The available FrameGrabber classes include OpenCVFrameGrabber (opencv_videoio),
+        // DC1394FrameGrabber, FlyCapture2FrameGrabber, OpenKinectFrameGrabber,
+        // PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
+        frameGrabber = new OpenCVFrameGrabber(0);
+
+        //frameGrabber.setFormat("mp4");
+        frameGrabber.setImageWidth(1280);
+        frameGrabber.setImageHeight(720);
+
+        logger.debug("Starting frame grabber");
+        try {
+            frameGrabber.start();
+            logger.debug("Started frame grabber with image width-height : {}-{}", frameGrabber.getImageWidth(), frameGrabber.getImageHeight());
+        } catch (FrameGrabber.Exception e) {
+            logger.error("Error when initializing the frame grabber", e);
+            throw new RuntimeException("Unable to start the FrameGrabber", e);
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            window.setVisible(true);
+        });
+
+        process();
+
+        logger.debug("Stopped frame grabbing.");
+    }
+
+    /**
+     * Private method which will be called to star frame grabbing and carry on processing the grabbed frames
+     */
+    private void process() {
+        running = true;
+        while (running) {
+            try {
+                // Here we grab frames from our camera
+                final Frame frame = frameGrabber.grab();
+
+                Map<Rect, Mat> detectedFaces = faceDetector.detect(frame);
+                Mat mat = toMatConverter.convert(frame);
+
+                detectedFaces.entrySet().forEach(rectMatEntry -> {
+                    String age = ageDetector.predictAge(rectMatEntry.getValue(), frame);
+                    CNNGenderDetector.Gender gender = genderDetector.predictGender(rectMatEntry.getValue(), frame);
+                    String caption = String.format("%s:[%s]", gender, age);
+                    logger.debug("Face's caption : {}", caption);
+
+                    rectangle(mat, new Point(rectMatEntry.getKey().x(), rectMatEntry.getKey().y()),
+                            new Point(rectMatEntry.getKey().width() + rectMatEntry.getKey().x(), rectMatEntry.getKey().height() + rectMatEntry.getKey().y()),
+                            Scalar.RED, 2, CV_AA, 0);
+
+                    int posX = Math.max(rectMatEntry.getKey().x() - 10, 0);
+                    int posY = Math.max(rectMatEntry.getKey().y() - 10, 0);
+                    putText(mat, caption, new Point(posX, posY), CV_FONT_HERSHEY_PLAIN, 1.0,
+                            new Scalar(255, 255, 255, 2.0));
+                });
+
+                // Show the processed mat in UI
+                Frame processedFrame = toMatConverter.convert(mat);
+
+                Graphics graphics = videoPanel.getGraphics();
+                BufferedImage resizedImage = ImageUtils.getResizedBufferedImage(processedFrame, videoPanel);
+                SwingUtilities.invokeLater(() -> {
+                    graphics.drawImage(resizedImage, 0, 0, videoPanel);
+                });
+            } catch (FrameGrabber.Exception e) {
+                logger.error("Error when grabbing the frame", e);
+            } catch (Exception e) {
+                logger.error("Unexpected error occurred while grabbing and processing a frame", e);
+            }
+        }
+    }
+
+    /**
+     * Stops and released resources attached to frame grabbing. Stops frame processing and,
+     */
+    public void stop() {
+        running = false;
+        try {
+            logger.debug("Releasing and stopping FrameGrabber");
+            frameGrabber.release();
+            frameGrabber.stop();
+        } catch (FrameGrabber.Exception e) {
+            logger.error("Error occurred when stopping the FrameGrabber", e);
+        }
+
+        window.dispose();
+    }
+
+    public static void main(String[] args) {
+        JavaCVExample javaCVExample = new JavaCVExample();
+
+        logger.info("Starting javacv example");
+        new Thread(javaCVExample::start).start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping javacv example");
+            javaCVExample.stop();
+        }));
+
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException ignored) { }
+    }
+}
